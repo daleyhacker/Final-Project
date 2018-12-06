9e8bc0eba20f569fb952b7cc5972332f74868976
daleyhacker <42852983+daleyhacker@users.noreply.github.com>
2018-12-06 10:39:33 -0500

    Updated from Brython Server: 12/6/2018 10:39:32 AM

--- Pong.py
+++ Pong.py
@@ -1,1 +1,46 @@
+"""
+Patrick Daley
+"""
 
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset, Frame
+from math import floor
+
+blue = Color(0x2EFEC8, 1.0)
+black = Color(0x000000, 1.0)
+pink = Color(0xFF00FF, 1.0)
+red = Color(0xFF5733, 1.0)
+white = Color(0xFFFFFF, 1.0)
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
+grey = Color(0xC0C0C0, 1.0)
+
+thinline = LineStyle(2, black)
+blkline = LineStyle(1, black)
+noline = LineStyle(0, white)
+coolline = LineStyle(1, grey)
+blueline = LineStyle(2, blue)
+redline = LineStyle(1, red)
+greenline = LineStyle(1, green)
+gridline = LineStyle(1, grey)
+grid=RectangleAsset(30,30,gridline,white)
+
+#--------------------------------------------------------------------------------------------------------------------
+#Player 1
+player1 = RectangleAsset(50,200, blkline, black)
+Sprite(player1)
+
+#Ball Sprite
+ball = CircleAsset(50, thinline, black)
+#Sprite(ball, 30, 30)
+
+#class Ball(Sprite):
+    #def 
+
+
+
+
+myapp = App()
+myapp.run()
